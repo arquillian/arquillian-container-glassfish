7bf829e1001e717a6e114e59f0cd72154915efc0
Dimitrij Drus <dimitrij.drus@secunet.com>
2016-02-02 13:22:42 +0100

    reverted changes in glassfish/managed_3_1 since they do not work in glassfish 3.x

--- glassfish-managed-3.1/src/main/java/org/jboss/arquillian/container/glassfish/managed_3_1/GlassFishServerControl.java
+++ glassfish-managed-3.1/src/main/java/org/jboss/arquillian/container/glassfish/managed_3_1/GlassFishServerControl.java
@@ -48,10 +48,6 @@ public class GlassFishServerControl {
         if (config.isDebug()) {
             args.add("--debug");
         }
-        if(config.isVerbose()) {
-        	args.add("--verbose");
-        }
-        
         int result = executeAdminDomainCommand("Starting container", admincmd, args);
         if (result > 0) {
             throw new LifecycleException("Could not start container");
