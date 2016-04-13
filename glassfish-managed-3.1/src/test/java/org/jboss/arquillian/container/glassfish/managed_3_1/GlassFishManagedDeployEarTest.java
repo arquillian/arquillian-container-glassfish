334a6c5d6f0bd470072e4fa65b795c2f2fd97277
thomas-mc-work <thomas.mc.work@gmail.com>
2016-04-13 13:45:31 +0200

    added integration test for the derby option

--- glassfish-managed-3.1/src/test/java/org/jboss/arquillian/container/glassfish/managed_3_1/GlassFishManagedDeployEarTest.java
+++ glassfish-managed-3.1/src/test/java/org/jboss/arquillian/container/glassfish/managed_3_1/GlassFishManagedDeployEarTest.java
@@ -16,16 +16,12 @@
  */
 package org.jboss.arquillian.container.glassfish.managed_3_1;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-
 import javax.servlet.annotation.WebServlet;
-
+import static org.hamcrest.core.IsEqual.equalTo;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -34,6 +30,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -59,7 +56,7 @@ public class GlassFishManagedDeployEarTest {
                 .addAsModule(ejb);
         return ear;
     }
-    
+
     @ArquillianResource
     private URL deploymentUrl;
 
@@ -72,6 +69,6 @@ public class GlassFishManagedDeployEarTest {
         BufferedReader in = new BufferedReader(new InputStreamReader(response.getInputStream()));
         final String result = in.readLine();
 
-        assertThat(result, equalTo("Hello"));
+        assertThat(result, equalTo("Hello-0"));
     }
 }
